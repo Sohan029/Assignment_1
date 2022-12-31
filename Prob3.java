9669b556809383c5be1429e06ac5d3f4e4597c82
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2022-12-31 18:02:14 +0530

    Prob3.java

---
+++ Prob3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Prob3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int len=sc.nextInt();
+        int bre=sc.nextInt();
+        System.out.println(len*bre);
+    }
+}
