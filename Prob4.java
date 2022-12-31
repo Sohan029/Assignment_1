4cb159298d39818c8c78a87e2b3a17408dca6b9f
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2022-12-31 18:08:29 +0530

    Prob4

---
+++ Prob4.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Prob4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cub=sc.nextInt();
+        System.out.println((cub*cub*cub));
+    }
+}
